306043657c21e1627f01cefd12705fea6df2daa7
Luke Carter <everythingsimpleinc1@gmail.com>
2025-10-03 13:14:17 +0100

    add

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,19 +1,35 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
+type AvatarContextValue = {
+  imageLoaded: boolean
+  setImageLoaded: (loaded: boolean) => void
+}
+
+const AvatarContext = React.createContext<AvatarContextValue>({
+  imageLoaded: false,
+  setImageLoaded: () => {},
+})
+
 const Avatar = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
-      className
-    )}
-    {...props}
-  />
-))
+>(({ className, ...props }, ref) => {
+  const [imageLoaded, setImageLoaded] = React.useState(false)
+
+  return (
+    <AvatarContext.Provider value={{ imageLoaded, setImageLoaded }}>
+      <div
+        ref={ref}
+        className={cn(
+          "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+          className
+        )}
+        {...props}
+      />
+    </AvatarContext.Provider>
+  )
+})
 Avatar.displayName = "Avatar"
 
 const AvatarImage = React.forwardRef<
@@ -21,6 +37,7 @@ const AvatarImage = React.forwardRef<
   React.ImgHTMLAttributes<HTMLImageElement>
 >(({ className, alt = "", ...props }, ref) => {
   const [imageError, setImageError] = React.useState(false)
+  const { setImageLoaded } = React.useContext(AvatarContext)
 
   // Don't render if there's no src
   if (!props.src) {
@@ -29,6 +46,11 @@ const AvatarImage = React.forwardRef<
 
   const handleError = () => {
     setImageError(true)
+    setImageLoaded(false)
+  }
+
+  const handleLoad = () => {
+    setImageLoaded(true)
   }
 
   // Don't render if image failed to load
@@ -39,9 +61,18 @@ const AvatarImage = React.forwardRef<
   return (
     <img
       ref={ref}
-      className={cn("aspect-square h-full w-full", className)}
+      className={cn("aspect-square h-full w-full object-cover", className)}
       alt={alt}
       onError={handleError}
+      onLoad={handleLoad}
+      style={{ 
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: 1
+      }}
       {...props}
     />
   )
@@ -51,16 +82,29 @@ AvatarImage.displayName = "AvatarImage"
 const AvatarFallback = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-muted",
-      className
-    )}
-    {...props}
-  />
-))
+>(({ className, ...props }, ref) => {
+  const { imageLoaded } = React.useContext(AvatarContext)
+
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        "flex h-full w-full items-center justify-center rounded-full bg-muted",
+        imageLoaded && "opacity-0",
+        className
+      )}
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: 0
+      }}
+      {...props}
+    />
+  )
+})
 AvatarFallback.displayName = "AvatarFallback"
 
 export { Avatar, AvatarImage, AvatarFallback }
